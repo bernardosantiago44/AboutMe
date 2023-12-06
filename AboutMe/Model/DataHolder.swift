a60426920c49bd47a0e390c104381f5f56968090
Bernardo Santiago <bernardosantiago44@gmail.com>
2023-12-06 12:09:05 -0600

    Final project

--- AboutMe/Model/DataHolder.swift
+++
@@ -1,8 +0,0 @@
-//
-//  DataHolder.swift
-//  AboutMe
-//
-//  Created by Bernardo Santiago Marin on 05/12/23.
-//
-
-import Foundation
--- AboutMe/Model/Project.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Project.swift
-//  AboutMe
-//
-//  Created by Bernardo Santiago Marin on 05/12/23.
-//
-
-import Foundation
